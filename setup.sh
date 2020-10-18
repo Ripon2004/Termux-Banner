c1452d3ca9721f00954f1289281009ca62dff64a
Ripon2004 <51786071+Ripon2004@users.noreply.github.com>
2020-10-18 11:19:29 +0600

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!usr/bin/bash
+#author : black_phish
+clear
+printf '\033[0;32m'
+figlet Black_Phish
+printf "\n"
+read -p " [+] Enter fullname: " name
+echo """if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found ]; then
+	command_not_found_handle() {
+		/data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
+	}
+fi
+cd /sdcard/
+clear
+printf '\\033[0;32m'
+figlet $name
+printf '\\n'
+# let us setup prompt
+export PS1='\[\e[32m\][\[\e[m\]\[\e[31m\]\u\[\e[m\]\[\e[33m\]@\[\e[m\]\[\e[32m\]\h\[\e[m\]:\[\e[36m\]\w\[\e[m\]\[\e[32m\]]\[\e[m\]\[\e[32;47m\]\\$\[\e[m\] '
+""">$PREFIX/etc/bash.bashrc
