617ae827f94050f777313a7d429e33e8d3930fa8
Ripon2004 <51786071+Ripon2004@users.noreply.github.com>
2020-11-20 18:54:36 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,11 +10,23 @@ echo """if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found
 		/data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
 	}
 fi
-cd /sdcard/
+home() {
+cd ~
+}
+udir() {
+cd $PREFIX
+}
+sdcard(){
+cd /sdcard
+}
+cls() {
 clear
 printf '\\033[0;32m'
 figlet $name
 printf '\\n'
+}
+cls
+cd /sdcard/
 # let us setup prompt
 export PS1='\[\e[32m\][\[\e[m\]\[\e[31m\]\u\[\e[m\]\[\e[33m\]@\[\e[m\]\[\e[32m\]\h\[\e[m\]:\[\e[36m\]\w\[\e[m\]\[\e[32m\]]\[\e[m\]\[\e[32;47m\]\\$\[\e[m\] '
 """>$PREFIX/etc/bash.bashrc
